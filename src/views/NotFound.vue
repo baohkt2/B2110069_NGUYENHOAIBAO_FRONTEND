541fdd7ff35c985e4fb82a301f39eedbae6b3e9b
baohkt2 <nbaocs13@gmail.com>
2023-10-20 17:18:33 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+
+import { RouterLink } from 'vue-router';
+<template> 
+    <div class="page">
+        <p>
+            Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">Trang chủ.</router-link>
+        </p>
+    </div>
+</template>
